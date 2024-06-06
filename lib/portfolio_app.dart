c990b589bb2d13cb362e26036cabf9e332820fee
Yash Adulkar <yash151099@gmail.com>
2024-06-06 15:16:46 +0530

    Code update

--- lib/portfolio_app.dart
+++ lib/portfolio_app.dart
@@ -7,6 +7,27 @@ class PortfolioApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 70.0,
+              backgroundImage: AssetImage('assets/Yash.jpg'),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("I am Yash Adulkar. "),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text("Today giving lec in Lets upgrade"),
+            SizedBox(
+              height: 10.0,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
